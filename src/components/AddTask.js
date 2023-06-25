bdcdfcf93473bc90325e46f553c76d67e718a67f
Rhys Morgan <rhysmoggs@hotmail.com>
2023-06-25 12:18:36 +0100

    add basic text state to task component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddTask() {
+  const [task, setTask] = useState('')
+
   const handleSubmit = (e) => {
     //prevents form from submitting
     e.preventDefault()
+    console.log(task)
   }
+
+  //check if form is valid
+
   return (
+    // handleSubmit function, add completed form to state
     <form onSubmit={handleSubmit}>
       <div>
         <label>
           Task:
           {/* <input type='text' value={state.value} onChange={handleChange} /> */}
-          <input type='text' value='Bob' />
+          <input
+            type='text'
+            plaecholder='Add Task'
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
         </label>
       </div>
       <div>
-        <label for='priority'>Priority:</label>
-        <select id='cars' name='cars'>
+        <label htmlFor='priority'>Priority:</label>
+        <select id='priority' name='priority'>
           <option value='low'>low</option>
           <option value='medium'>medium</option>
           <option value='high'>high</option>
@@ -25,10 +37,9 @@ function AddTask() {
       <div>
         <label>
           Day:
-          <input type='date' value='Tuesday' />
+          <input type='date' />
         </label>
       </div>
-      {/* onSubmit, add form to state */}
       <input type='submit' value='Submit' />
     </form>
   )
